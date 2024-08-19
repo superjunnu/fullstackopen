3baa5b27137e7b768fb0965ec72d7ee44aa90c5f
superjunnu <junnu144@gmail.com>
2024-08-19 20:50:22 +0300

    tehtävä 1.6

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,9 +1,49 @@
 import { useState } from "react";
 
+const Header = ({ title }) => {
+  return <h2>{title}</h2>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const RenderText = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  );
+};
+
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodBtnClicked = () => {
+    const updatedValue = good + 1;
+    setGood(updatedValue);
+  };
+  const neutralBtnClicked = () => {
+    const updatedValue = neutral + 1;
+    setNeutral(updatedValue);
+  };
+  const badBtnClicked = () => {
+    const updatedValue = bad + 1;
+    setBad(updatedValue);
+  };
+
   return (
     <>
-      <p>moi</p>
+      <Header title="Give Feedback" />
+      <Button handleClick={goodBtnClicked} text="Good" />
+      <Button handleClick={neutralBtnClicked} text="Neutral" />
+      <Button handleClick={badBtnClicked} text="Bad" />
+      <Header title="Statistics" />
+      <RenderText text="good" value={good} />
+      <RenderText text="neutral" value={neutral} />
+      <RenderText text="bad" value={bad} />
     </>
   );
 }
