a1727bd475526fab30fa92cf9870d671c5d22a48
superjunnu <junnu144@gmail.com>
2024-08-23 12:35:39 +0300

    osa2 kansion pohjustus

---
+++ osa2/kurssitiedot2/src/App.jsx
@@ -0,0 +1,35 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    id: 1,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  const Course = () => {
+    return <div>App</div>;
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
