3b43f798e387a6596aa092c36428c41aa44010c3
superjunnu <junnu144@gmail.com>
2024-08-26 08:59:13 +0300

    tehtävä 2.1

--- osa2/kurssitiedot2/src/App.jsx
+++ osa2/kurssitiedot2/src/App.jsx
@@ -1,3 +1,33 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
@@ -19,10 +49,6 @@ const App = () => {
         id: 3,
       },
     ],
-  };
-
-  const Course = () => {
-    return <div>App</div>;
   };
 
   return (
