baacc6e917a6df72956983ae464381447b84654f
superjunnu <junnu144@gmail.com>
2024-08-27 15:19:07 +0300

    tehtävä 2.5 muutos kurssit näkyviin map-metodilla

--- osa2/kurssitiedot2/src/App.jsx
+++ osa2/kurssitiedot2/src/App.jsx
@@ -49,8 +49,11 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      <Course course={courses[0]} />
-      <Course course={courses[1]} />
+      {/* <Course course={courses[0]} />
+      <Course course={courses[1]} /> */}
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
