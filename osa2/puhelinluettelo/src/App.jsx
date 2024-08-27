11bb4d9044968feaca205902c793737878405f06
superjunnu <junnu144@gmail.com>
2024-08-27 13:56:14 +0300

    tehtävä 2.6

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -4,19 +4,40 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addNewName = (event) => {
+    event.preventDefault();
+
+    const newNameObject = {
+      name: newName,
+    };
+
+    const personsCopy = [...persons];
+
+    setPersons(personsCopy.concat(newNameObject));
+    setNewName("");
+  };
+
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
     </div>
   );
 };
