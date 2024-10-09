5a15c1b17060fb545681bd4d441de2c8afa5d0f3
superjunnu <junnu144@gmail.com>
2024-10-09 15:04:10 +0300

    tehtävät 3.3 ja 3.4

--- osa3/index.js
+++ osa3/index.js
@@ -48,6 +48,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
   const today = new Date();
 
