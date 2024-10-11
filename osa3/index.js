3d617e2c25cea94165034f87c5409c16e1123219
superjunnu <junnu144@gmail.com>
2024-10-11 10:37:31 +0300

    tehtävä 3.5

--- osa3/index.js
+++ osa3/index.js
@@ -5,26 +5,26 @@ const PORT = 3001;
 app.use(express.json());
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+  // {
+  //   id: 1,
+  //   name: "Arto Hellas",
+  //   number: "040-123456",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Ada Lovelace",
+  //   number: "39-44-5323523",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Dan Abramov",
+  //   number: "12-43-234345",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Mary Poppendieck",
+  //   number: "39-23-6423122",
+  // },
 ];
 
 app.get("/", (request, response) => {
@@ -41,11 +41,7 @@ app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  person ? response.json(person) : response.status(404).end();
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -61,6 +57,29 @@ app.get("/info", (request, response) => {
     Phonebook has info for ${persons.length} people
     <br><br>
     ${today}`);
+});
+
+const generateId = () => {
+  const randomId = Math.trunc(Math.random() * Date.now());
+
+  const addId = persons.length > 0 ? randomId : 0;
+  return addId;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  console.log(person);
+
+  response.json(person);
 });
 
 app.listen(PORT, () => {
