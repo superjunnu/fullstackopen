493efa898d0a049248988c4aafac0cec8d2221b5
superjunnu <junnu144@gmail.com>
2024-10-16 10:23:46 +0300

    tehtävä 3.9

--- osa3/index.js
+++ osa3/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-
 app.use(morgan("tiny"));
+app.use(cors());
 
 let persons = [
   {
