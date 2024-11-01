17c3201793c22f1ae6849780d36c208437821412
superjunnu <junnu144@gmail.com>
2024-11-01 09:06:52 +0200

    tehtävä 3.13

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const app = express();
+require("dotenv").config();
+const Person = require("./models/person");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -9,69 +11,69 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 // Hakee kaikki sen hetken henkilöt
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+// app.get("/api/persons", (request, response) => {
+//   response.json(persons);
+// });
 
-// Hakee henkilön ID:n avulla
+// // Hakee henkilön ID:n avulla
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+// app.get("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   const person = persons.find((person) => person.id === id);
 
-  person ? response.json(person) : response.status(404).end();
-});
+//   person ? response.json(person) : response.status(404).end();
+// });
 
-// Poistaa henkilön ID:n perusteella
+// // Poistaa henkilön ID:n perusteella
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
-});
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   persons = persons.filter((person) => person.id !== id);
+//   response.status(204).end();
+// });
 
-app.get("/info", (request, response) => {
-  const today = new Date();
+// app.get("/info", (request, response) => {
+//   const today = new Date();
 
-  response.send(`
-    Phonebook has info for ${persons.length} people
-    <br><br>
-    ${today}`);
-});
+//   response.send(`
+//     Phonebook has info for ${persons.length} people
+//     <br><br>
+//     ${today}`);
+// });
 
 // Luo random ID lisättävälle henkilölle
 
-const generateId = () => {
-  const randomId = Math.trunc(Math.random() * Date.now());
+// const generateId = () => {
+//   const randomId = Math.trunc(Math.random() * Date.now());
 
-  const addId = persons.length > 0 ? randomId : 0;
-  return addId;
-};
+//   const addId = persons.length > 0 ? randomId : 0;
+//   return addId;
+// };
 
 // Luodaan token POST-tietoa varten
 
@@ -89,35 +91,43 @@ app.use(
 
 // Lisää uuden henkilön jos tietyt ehdot täyttyvät
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
+// app.post("/api/persons", (request, response) => {
+//   const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({ error: "Name missing" });
-  }
-  if (!body.number) {
-    return response.status(400).json({ error: "Number missing" });
-  }
+//   if (!body.name) {
+//     return response.status(400).json({ error: "Name missing" });
+//   }
+//   if (!body.number) {
+//     return response.status(400).json({ error: "Number missing" });
+//   }
 
-  // Uusi luotava henkilö olio
+//   // Uusi luotava henkilö olio
 
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
+//   const person = {
+//     id: generateId(),
+//     name: body.name,
+//     number: body.number,
+//   };
 
-  // Muuttaa nimet vertailua varten ja suorittaa tarkastuksen
+//   // Muuttaa nimet vertailua varten ja suorittaa tarkastuksen
 
-  const checkName = persons.map((person) => person.name.toLowerCase());
+//   const checkName = persons.map((person) => person.name.toLowerCase());
 
-  if (checkName.includes(person.name.toLowerCase())) {
-    return response.status(400).json({ error: "Person already exists" });
-  } else {
-    persons = persons.concat(person);
+//   if (checkName.includes(person.name.toLowerCase())) {
+//     return response.status(400).json({ error: "Person already exists" });
+//   } else {
+//     persons = persons.concat(person);
 
-    response.json(person);
-  }
+//     response.json(person);
+//   }
+// });
+
+// Getting all persons
+
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.listen(PORT, () => {
