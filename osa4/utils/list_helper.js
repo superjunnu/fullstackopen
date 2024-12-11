3880d042be8c97444809f3db85411a94852b6de7
superjunnu <junnu144@gmail.com>
2024-12-11 12:16:48 +0200

    tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+
+  const blogLikes = blogs.map((blogs) => blogs.likes);
+
+  return blogLikes.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
