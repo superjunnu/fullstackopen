27b263b2c6b3ff5991ffe13b8503fbc1e350bf38
superjunnu <junnu144@gmail.com>
2024-12-11 13:41:27 +0200

    tehtävä 4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -12,7 +12,28 @@ const totalLikes = (blogs) => {
   return blogLikes.reduce(reducer, 0);
 };
 
+// const blogs = [
+//   { title: "Blog 1", author: "Author 1", likes: 5 },
+//   { title: "Blog 2", author: "Author 2", likes: 15 },
+//   { title: "Blog 3", author: "Author 3", likes: 10 },
+// ];
+
+const favoriteBlog = (blogs) => {
+  const blogsLikes = blogs.map((blogs) => blogs.likes);
+  const getIndexOfMostBlogLikes = blogsLikes.indexOf(Math.max(...blogsLikes));
+  const hasMostBlogLikes = blogs[getIndexOfMostBlogLikes];
+
+  return {
+    title: hasMostBlogLikes.title,
+    author: hasMostBlogLikes.author,
+    likes: hasMostBlogLikes.likes,
+  };
+};
+
+// console.log(favoriteBlog(blogs));
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
