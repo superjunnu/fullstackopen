e27fbfda8bcac8225383d6e54b4bddf8001fbb4a
superjunnu <junnu144@gmail.com>
2025-01-09 19:26:08 +0200

    tehtävä 4.10

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -29,10 +29,31 @@ test("should return blogs with right unique id", async () => {
   const blogs = await helper.blogsInDb();
 
   blogs.forEach((blog) => {
-    console.log(blog.id);
-
     assert(blog.id);
   });
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((n) => n.title);
+
+  assert(titles.includes("Type wars"));
 });
 
 after(async () => {
