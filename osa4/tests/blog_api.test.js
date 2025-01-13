c16c958b8b4dabefe23178778e126e0b070e12e3
superjunnu <junnu144@gmail.com>
2025-01-13 11:39:05 +0200

    tehtävä 4.14

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, describe, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -12,111 +12,148 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe("when there is some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
 
-  assert.strictEqual(response.body.length, helper.initialBlogs.length);
-});
-
-test("should return blogs with right unique id", async () => {
-  const blogs = await helper.blogsInDb();
-
-  blogs.forEach((blog) => {
-    assert(blog.id);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-  };
+describe("viewing a specific blog", () => {
+  test("should return blogs with right unique id", async () => {
+    const blogs = await helper.blogsInDb();
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    blogs.forEach((blog) => {
+      assert(blog.id);
+    });
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+    };
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const titles = blogsAtEnd.map((n) => n.title);
+    const blogsAtEnd = await helper.blogsInDb();
 
-  assert(titles.includes("Type wars"));
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((n) => n.title);
+
+    assert(titles.includes("Type wars"));
+  });
+
+  test("new blog without likes property will be set to 0", async () => {
+    const newBlog = {
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+    const likeLikes = blogsAtEnd.map((n) => n.likes);
+
+    assert(likeLikes.includes(0));
+  });
+
+  test("new blog without title property will not be added", async () => {
+    const newBlog = {
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      author: "Robert C. Martin",
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+
+  test("new blog without url property will not be added", async () => {
+    const newBlog = {
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
 });
 
-test("new blog without likes property will be set to 0", async () => {
-  const newBlog = {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-  };
+describe("delete a blog", () => {
+  test("a blog can be deleted", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  const blogsAtEnd = await helper.blogsInDb();
+    const blogsAtEnd = await helper.blogsInDb();
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
 
-  const likeLikes = blogsAtEnd.map((n) => n.likes);
+    const titles = blogsAtEnd.map((r) => r.title);
 
-  assert(likeLikes.includes(0));
+    assert(!titles.includes(blogToDelete.title));
+  });
 });
 
-test("new blog without title property will not be added", async () => {
-  const newBlog = {
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    author: "Robert C. Martin",
-  };
+describe("update of a blog", () => {
+  test("the information of an individual blog post is updated", async () => {
+    const blogsAtStart = await helper.blogsInDb();
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogToView = blogsAtStart[0];
 
-  const blogsAtEnd = await helper.blogsInDb();
+    const newBlog = {
+      title: blogsAtStart[0].title,
+      author: blogsAtStart[0].author,
+      url: blogsAtStart[0].url,
+      likes: 2000000,
+    };
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-});
+    await api
+      .put(`/api/blogs/${blogToView.id}`)
+      .send(newBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-test("new blog without url property will not be added", async () => {
-  const newBlog = {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-  };
+    const blogsAtEnd = await helper.blogsInDb();
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
 
-  const blogsAtEnd = await helper.blogsInDb();
+    const beforeLikes = blogsAtStart.map((n) => n.likes);
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-});
+    const afterLikes = blogsAtEnd.map((n) => n.likes);
 
-test("a blog can be deleted", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
-
-  const titles = blogsAtEnd.map((r) => r.title);
-
-  assert(!titles.includes(blogToDelete.title));
+    assert(!afterLikes.includes(beforeLikes));
+  });
 });
 
 after(async () => {
