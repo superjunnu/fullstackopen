c131eb0ecebbc0e4bd9a448574052f7e13f80347
superjunnu <junnu144@gmail.com>
2025-01-13 11:29:54 +0200

    tehtävä 4.13

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -6,6 +6,15 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
+
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
@@ -22,6 +31,11 @@ blogsRouter.post("/", async (request, response) => {
     const savedBlog = await blog.save();
     response.status(201).json(savedBlog);
   }
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogsRouter;
--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -104,6 +104,21 @@ test("new blog without url property will not be added", async () => {
   assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
 });
 
+test("a blog can be deleted", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+  const titles = blogsAtEnd.map((r) => r.title);
+
+  assert(!titles.includes(blogToDelete.title));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
