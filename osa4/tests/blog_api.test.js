a4612d0ed1607eb324973f64978a100fb4b4e8ef
superjunnu <junnu144@gmail.com>
2025-01-16 11:04:24 +0200

    tehtävä 4.15

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -156,6 +156,39 @@ describe("update of a blog", () => {
   });
 });
 
+describe("when there is initially one user in db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "testi",
+      name: "testaaja",
+      password: "test",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
