ba489a1aeae254667075fc8175335826f97cb3b4
superjunnu <junnu144@gmail.com>
2025-01-16 16:05:07 +0200

    tehtävä 4.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,8 +1,13 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
@@ -18,18 +23,24 @@ blogsRouter.get("/:id", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ? body.likes : 0,
+    user: user._id,
   });
 
   if (!body.title || !body.url) {
     response.status(400).json({ error: "title or url is missing" });
   } else {
     const savedBlog = await blog.save();
-    response.status(201).json(savedBlog);
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.json(savedBlog);
   }
 });
 
